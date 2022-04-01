d79de29449de0216c7bdd047d1c3274bee89f098
Jose <josepls.v@hotmail.com>
2022-04-01 13:50:50 -0700

    first commit

---
+++ BankingEncapsulation/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankingEncapsulation
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+        public double Balance
+        {
+            get { return _balance; }
+            set { _balance = value; }
+        }
+        public void Deposit(double b)
+        {
+            _balance += b;
+        }
+        public double GetBalance()
+        {
+            return Balance;
+        }
+    }
+}
---
+++ BankingEncapsulation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace BankingEncapsulation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var bankA = new BankAccount();
+            Console.WriteLine($"Your balance is ${bankA.Balance}\nEnter how much money you want to deposit...");
+            Console.Write("$");
+            bankA.Deposit(double.Parse(Console.ReadLine()));
+            Console.WriteLine($"Your money has been deposited. Your balance is:\n${bankA.Balance}");
+        }
+    }
+}
